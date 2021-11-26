6249ef452f33332d564d7c2a81f873ca413cfef9
Fedor <fedor@DevelopmentOnTheEdge.com>
2021-11-27 02:41:46 +0700

    start of project

---
+++ src/main/java/ru/biosoft/uscience/bpmn/tasks/PrintVariablesTask.java
@@ -0,0 +1,16 @@
+package ru.biosoft.uscience.bpmn.tasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+/**
+ * Utility tasks to print workflow variables.
+ */
+public class PrintVariablesTask implements JavaDelegate 
+{
+	public void execute(DelegateExecution execution) throws Exception 
+    {
+		System.out.println("Task PrintVariablesTask, variables: ");
+		System.out.println(execution.getVariables());
+	}
+}
---
+++ src/test/java/ru/biosoft/uscience/bpmn/EngineTest.java
@@ -0,0 +1,90 @@
+package ru.biosoft.uscience.bpmn;
+
+import java.util.Map;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.form.FormData;
+import org.camunda.bpm.engine.repository.Deployment;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EngineTest 
+{
+	static ProcessEngine processEngine;
+	static Deployment deployment;
+	static ProcessInstance processInstance;
+	static String processDefinitionId;
+
+	@Test
+	public void _1_initProcessEngine()
+	{
+		processEngine = ProcessEngineConfiguration
+				.createProcessEngineConfigurationFromResourceDefault()
+				.buildProcessEngine();
+	}
+
+	@Test
+	public void _2_deployWorkflow()
+	{
+		deployment = processEngine.getRepositoryService()
+				     .createDeployment()
+				     .addInputStream("printVariables.bpmn", this.getClass().getClassLoader().getResourceAsStream("printVariables.bpmn"))
+				     .deploy();
+		
+	    List<ProcessDefinition> definitions = processEngine.getRepositoryService()
+	    		.createProcessDefinitionQuery()
+	    		.deploymentId(deployment.getId())
+	    		.list();
+	    		
+	    System.out.println("!!!Definitions: " + definitions);	    
+		
+	    processDefinitionId = definitions.get(0).getId();
+	    System.out.println("!!!Definitions: " + definitions);	    
+	}
+
+	@Test
+	public void _3_workflowParameters()
+	{
+		FormData fd = processEngine.getFormService()
+				.getStartFormData(processDefinitionId);
+		
+		Map<String, Object> vars = processEngine.getFormService()
+				.getStartFormVariables(processDefinitionId);
+		
+	    System.out.println("!!!Form data: " + fd);	    
+	}
+	
+	@Test
+	public void _4_startWorkflow()
+	{
+		// init variables
+	    Map<String, Object> variables = new Hashtable<String, Object>();
+
+		// start the process instance
+//	    processInstance = processEngine.getRuntimeService()
+//	    		.startProcessInstanceByKey("testWorkflowId", variables);
+	    
+		ProcessInstance pi = processEngine.getRuntimeService()
+	    		.startProcessInstanceById(processDefinitionId, variables);
+
+	    System.out.println("!!!Process instance: " + pi);	    
+	}
+	
+	@Test
+	public void _9_removeWorkflow()
+	{
+//		processEngine.getRepositoryService().deleteDeployment("601", true);
+
+		processEngine.getRepositoryService()
+			.deleteDeployment(deployment.getId(), true);
+	}
+		
+}
