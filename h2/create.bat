1477ec4e35b02c3c26e87ec4852b63ff0acdf07f
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2021-11-27 10:49:52 +0700

    version with stdin

--- h2/create.bat
+++ h2/create.bat
@@ -1,2 +1,3 @@
 cd ..
-mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args="-url 'jdbc:h2:~/pilot;INIT=runscript from 'h2/h2_engine_7.16.0.sql'' -user sa"
+#mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args="-url 'jdbc:h2:~/pilot;INIT=runscript from 'h2/h2_engine_7.16.0.sql'' -user sa"
+mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args='-url "jdbc:h2:~/pilot" -user sa' < h2/h2_engine_7.16.0.sql
