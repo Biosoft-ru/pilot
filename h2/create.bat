04190d7a75a64b4b7a3d8e9f2061e56828e127ac
Fedor <fedor@DevelopmentOnTheEdge.com>
2021-11-27 14:28:41 +0700

    recreate h2 db with identity

--- h2/create.bat
+++ h2/create.bat
@@ -1,3 +1,3 @@
 cd ..
-#mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args="-url 'jdbc:h2:~/pilot;INIT=runscript from 'h2/h2_engine_7.16.0.sql'' -user sa"
-mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args='-url "jdbc:h2:./h2/pilot" -user sa' < h2/h2_engine_7.16.0.sql
+mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args="-url 'jdbc:h2:./h2/pilot' -user sa" < h2/h2_engine_7.16.0.sql 
+mvn exec:java -Dexec.mainClass=org.h2.tools.Shell -Dexec.args="-url 'jdbc:h2:./h2/pilot' -user sa" < h2/h2_identity_7.16.0.sql
