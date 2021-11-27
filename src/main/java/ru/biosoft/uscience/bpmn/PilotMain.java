964cc58386c9d60c5df25faa386303393409bfa9
Fedor <fedor@DevelopmentOnTheEdge.com>
2021-11-28 00:55:17 +0700

    first draft that can start workflow

--- src/main/java/ru/biosoft/uscience/bpmn/PilotMain.java
+++ src/main/java/ru/biosoft/uscience/bpmn/PilotMain.java
@@ -1,6 +1,8 @@
 package ru.biosoft.uscience.bpmn;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Base64;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.logging.Level;
@@ -13,14 +15,23 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.h2.tools.Server;
+import org.yaml.snakeyaml.Yaml;
 
 public class PilotMain 
 {
-    static long start;
+	// Keys for YAML file
+	public static final String WORKFLOW_KEY = "workflow";  
+	
+	static long start;
     protected static Logger log;
     protected static CommandLine cmd;
+    protected static File workDir;
+    protected static Map<String, Object> yamlMap;
+    
 	protected static Server server;
 	protected static BpmnService bpmnService;
+	protected static String processDefinitionKey;
+	protected static String processInstanceId;
 	
 	public static void main(String[] args) 
 	{
@@ -35,8 +46,22 @@ public class PilotMain
 
         	server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifExists").start();
         	bpmnService = new CamundaBpmnServiceImpl();
-        	
         	log.log(Level.INFO, "BPMN engine started, t=" + (System.currentTimeMillis()-start));
+        	
+        	String workflow = "default.bpmn";
+        	if( yamlMap.containsKey(WORKFLOW_KEY) )
+        		workflow = (String) yamlMap.get(WORKFLOW_KEY);
+        	
+        	long deployStart = System.currentTimeMillis();
+        	processDefinitionKey = bpmnService.deployModel(workflow, 
+        			PilotMain.class.getClassLoader().getResourceAsStream(workflow));
+        	log.log(Level.INFO, "BPMN workflow loaded: " + workflow + ", t=" + (System.currentTimeMillis()-deployStart));
+
+            processInstanceId = bpmnService.startProcess(processDefinitionKey, yamlMap);
+        	log.log(Level.INFO, "Process started, id: " + processInstanceId);
+
+        	bpmnService.deleteModel(processDefinitionKey);
+        	log.log(Level.INFO, "Process completed, delete model: " + processInstanceId);
         }
         catch(Throwable t)
         {
@@ -58,7 +83,7 @@ public class PilotMain
 	{
 		Options options = new Options();
 		options.addRequiredOption("y", "yaml64",  true, "YAML file (encoded in BASE64) with task configuraton for pilot");
-		options.addOption("c", "clean", false, "Clean working directory for pilot before the task execution");
+		options.addOption("c", "cleanDir", false, "Clean working directory for pilot before the task execution");
 		options.addOption("w", "workDir", false, "Working directory. By default it is ./tmp");
 
 		try
@@ -83,7 +108,7 @@ public class PilotMain
 		if( cmd.hasOption("workDir") )
 			workDirPath = cmd.getOptionValue("workDir");	
 		
-		File workDir = new File(workDirPath);
+		workDir = new File(workDirPath);
 		String path = null;
 		try
 		{
@@ -94,7 +119,12 @@ public class PilotMain
 			log.log(Level.SEVERE, "Working dir path error: " + e, e);
 		}
 		
-		if (!workDir.exists() )
+		if( workDir.exists() )
+		{
+			if( cmd.hasOption("cleanDir") )
+				cleanDir(workDir, false);	
+		}
+		else
 		{
 			if( ! workDir.mkdirs() )
 			{
@@ -103,58 +133,51 @@ public class PilotMain
 			}
 		}		
 
-        log.log(Level.INFO, "Working dir: " + workDirPath + ", path: " + path);
+        // create yaml file from string parameter in base64
+        String base64 = cmd.getOptionValue("yaml64");	
+        String yamlStr = new String(Base64.getDecoder().decode(base64));
+        try
+        {
+        	FileWriter writer = new FileWriter(new File(workDir, "task.yaml"));
+        	writer.append(yamlStr);
+        	writer.close();
+        }
+        catch(Exception e)
+        {
+            log.log(Level.SEVERE, "Can not write yaml file for the task: " + e.getMessage(), e);
+            System.exit(-1);
+        }
 
+        yamlMap = (new Yaml()).load(yamlStr);
+        System.out.println(yamlMap);        
+        
+        log.log(Level.INFO, "Working dir is ready, path: " + path + System.lineSeparator() + "YAML: " + yamlMap);
 	}
 
+	protected static void cleanDir(File dir, boolean deleteDir) 
+	{
+		String path = null;
+		try
+		{
+			path = dir.getCanonicalPath();
+			log.log(Level.INFO, "Clean working dir: " + path);
+
+			for(File f : dir.listFiles() ) 
+			{
+				if( f.isDirectory() )
+					cleanDir(f, true);
+				else
+					f.delete();
+			}
+		
+			if( deleteDir )
+				dir.delete();
+		}
+		catch(Exception e)
+		{
+            log.log(Level.SEVERE, "Can not clean working directory: " + path);
+            System.exit(-1);
+		}
+	} 
 	
-	
 }
-
-
-/*
-static Server server;
-static BpmnService bpmnService;
-static String processDefinitionKey;
-static String processInstanceId;
-
-static String WORKFLOW = "printVariables.bpmn";
-
-
-@Test
-public void _1_init()
-{
-	long start = System.currentTimeMillis(); 
-	bpmnService = new CamundaBpmnServiceImpl();
-	System.out.println("BPMN service loading: " + (System.currentTimeMillis()-start));
-
-	start = System.currentTimeMillis(); 
-	processDefinitionKey = bpmnService.deployModel(WORKFLOW, 
-			getClass().getClassLoader().getResourceAsStream(WORKFLOW));
-	System.out.println("Deploy model: " + (System.currentTimeMillis()-start));
-}
-
-@Test
-public void _2_startWorkflow()
-{
-	// init variables
-    Map<String, Object> variables = new Hashtable<String, Object>();
-    variables.put("e-mail", "test@test.com");
-    
-    processInstanceId = bpmnService.startProcess(processDefinitionKey, variables);
-}
-
-@Test
-public void _8_removeWorkflow()
-{
-
-	bpmnService.deleteModel(processDefinitionKey);
-	bpmnService.deleteModel("Process_printVariables");		
-}
-
-@Test
-public void _9_stopH2Server()
-{
-	server.stop();
-}
-*/
